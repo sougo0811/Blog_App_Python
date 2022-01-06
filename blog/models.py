91532b5410db8539723946ab364deb204dd00bfb
sougo0811 <sougo2813@outlook.jp>
2022-01-07 01:05:59 +0900

    ブログアプリ機能追加 （追加、編集、保存） レイアウト変更

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,11 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=30)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="作成者")
+    title = models.CharField(max_length=30,verbose_name="タイトル")
+    text = models.TextField(verbose_name="内容")
+    created_date = models.DateTimeField(default=timezone.now, verbose_name="作成日")
+    published_date = models.DateTimeField(blank=True, null=True, verbose_name="公開日")
 
 
     def publish(self):
